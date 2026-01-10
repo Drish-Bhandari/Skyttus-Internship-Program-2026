0b16cf686e5386f58848c89196af5c2de1ca81fd
Drish Bhandari <160303939+Drish-Bhandari@users.noreply.github.com>
2026-01-10 13:46:01 +0530

    Assessment 2

---
+++ Assessment 2/program.py
@@ -0,0 +1,90 @@
+# 1.Calculate the remainder of two number
+# num1 = 20
+# num2 = 5
+# remainder = num1 % num2
+# print("Remmainder = ", remainder)
+
+# 2.check if number is odd or even
+# num = int(input("Enter number to check even or odd: "))
+# if num % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+# 3.Compare two number and print the largest one
+# num1 = 10
+# num2 = 20
+# if num1 > num2:
+#     print(f"{num1} is largest")
+# else:
+#     print(f"{num2} is largest")
+
+# 4.write a program to calculate the square and cube of number
+# num = int(input("Enter number to calulate square & cube: "))
+# square = num*num
+# cube = num*num*num
+# print(f"Square={square}  Cube={cube}")
+
+# 5.check if two number entered is equal
+# num1 = int(input("Enter num 1:"))
+# num2 = int(input("Enter num 2:"))
+# if num1 == num2:
+#     print("Equal")
+# else:
+#     print("Not equal")
+
+# 6.Take two number and print ture if both are positive else print flase
+# num1 = int(input("Enter num 1:"))
+# num2 = int(input("Enter num 2:"))
+# if (num1 and num2) > 0:
+#     print("True")
+# else:
+#     print("False")
+
+# 7.Write a program to convet float to integer
+# num = 3.14
+# integer = int(num)
+# print("Float to integer=",integer)
+
+# 8.Take a number as string, convert to int, and multiply by 10
+# num = input("Enter number: ")
+# result = int(num) * 10
+# print(f"{num} * 10 = {result}")
+
+# 9.Write a program that uses and & or operater to check multiple condition
+# age = 25
+# has_license = True
+# has_car = False
+
+# # Condition 1: Check if a person is over 18 AND has a license
+# if age > 18 and has_license:
+#     print("Condition 1: Person is an adult and has a license.")
+# else:
+#     print("Condition 1: Person does not meet both criteria.")
+
+# # Condition 2: Check if a person has a license OR has a car
+# if has_license or has_car:
+#     print("Condition 2: Person has a license or a car.")
+# else:
+#     print("Condition 2: Person has neither a license nor a car.")
+
+# # Check if the person is over 18 AND (has a license OR has a car)
+# if age > 18 and (has_license or has_car):
+#     print("Condition 3: Person is an adult AND has a license or a car.")
+# else:
+#     print("Condition 3: Person does not meet the combined criteria.")
+
+# 10.Divide two number and print the quotient and remainder seperatly
+dividend = 19
+divisor = 5
+# Calculate quotient
+quotient = dividend / divisor
+# Calculate remainder 
+remainder = dividend % divisor
+
+# Print the results
+print("Dividend:", dividend)
+print("Divisor:", divisor)
+print("Quotient:", quotient)
+print("Remainder:", remainder)
+
